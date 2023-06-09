de71764fd7b0f7f749329a289459ba2c22c6eade
Faiza Huda <58957309+FeFiFoFaiza@users.noreply.github.com>
2023-06-09 08:35:48 -0400

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,63 +1,110 @@
-// import React from 'react';
-// import SocketComponent from './SocketComponent';
+// // import React from 'react';
+// // import SocketComponent from './SocketComponent';
+
+// // function App() {
+// //   return (
+// //     <h1>hello world</h1>
+// //     // <div>
+// //     //   <h1>My App</h1>
+// //     //   {/* <SocketComponent /> */}
+// //     // </div>
+// //   );
+// // }
+
+// // export default App;
+
+// import React from "react"
+// import { BrowserRouter as Router, Routes, Route }
+//     from 'react-router-dom';
+
+// // functions and classes from other files
+// import Player from './player'
 
 // function App() {
+//   //  state = {
+//   //   name: ""
+//   // };
+
+//   // componentDidMount() {
+//   //   fetch("http://localhost:3000")
+//   //     .then(res => res.json())
+//   //     .then(data => this.setState({ name: data.name }))
+//   // };
+
+//   // render() {
+//   //   return (
+//   //     <h1>Hello {this.state.name}!</h1>
+//   //   )
+//   // };
+//   // const cards = ["red", "orange", "light green"]
+
 //   return (
-//     <h1>hello world</h1>
 //     // <div>
-//     //   <h1>My App</h1>
-//     //   {/* <SocketComponent /> */}
+//     //   {cards.map(card => (
+//     //     <>
+//     //       <h1>{card}</h1>
+          
+//     //     </>
+//     //   ))}
 //     // </div>
+//     // <Router>
+//     //   <Routes>
+//     //     <Route path='/blogs' element={<Blogs />} />
+//     //   </Routes>
+//     // </Router>
+//     <>
+//       <h1>testing</h1>
+//       <Player />
+//     </>
+    
 //   );
 // }
 
-// export default App;
+// export default App
 
-import React from "react"
-import { BrowserRouter as Router, Routes, Route }
-    from 'react-router-dom';
+import React from 'react';
 
-// functions and classes from other files
-import Player from './player'
-
-function App() {
-  //  state = {
-  //   name: ""
-  // };
-
-  // componentDidMount() {
-  //   fetch("http://localhost:3000")
-  //     .then(res => res.json())
-  //     .then(data => this.setState({ name: data.name }))
-  // };
-
-  // render() {
-  //   return (
-  //     <h1>Hello {this.state.name}!</h1>
-  //   )
-  // };
-  // const cards = ["red", "orange", "light green"]
-
+const HomeScreen = () => {
   return (
-    // <div>
-    //   {cards.map(card => (
-    //     <>
-    //       <h1>{card}</h1>
-          
-    //     </>
-    //   ))}
-    // </div>
-    // <Router>
-    //   <Routes>
-    //     <Route path='/blogs' element={<Blogs />} />
-    //   </Routes>
-    // </Router>
-    <>
-      <h1>testing</h1>
-      <Player />
-    </>
-    
+    <div>
+      <button id="joinButton">Join</button>
+      <button id="createButton">Create</button>
+    </div>
   );
-}
+};
 
-export default App
+const CreateGame = () => {
+  return <div id="newGameCode">DNE</div>;
+};
+
+const JoinGame = () => {
+  return (
+    <div>
+      <div>
+        <label htmlFor="inputPlayerName">Your Name:</label>
+        <input type="text" id="inputPlayerName" />
+      </div>
+
+      <div>
+        <label htmlFor="inputGameId">Game ID:</label>
+        <input type="text" id="inputGameId" />
+      </div>
+
+      <div>
+        <button id="joinGame">Join Game</button>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <HomeScreen />
+      <CreateGame />
+      <JoinGame />
+    </div>
+  );
+};
+
+export default App;
